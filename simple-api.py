1c4ec30af49ce947f86d4c6a553d1de1baccf3ac
nima-farnoodian <nima.farnoodian@outlook.com>
2022-03-27 03:20:11 +0200

    issue 2: A very simple flask HELLOWORLD project added

---
+++ simple-api.py
@@ -0,0 +1,11 @@
+from flask import Flask
+ai_api = Flask(__name__)
+
+
+@ai_api.route('/',methods=["GET"])
+def hello_world():
+    return 'A hello world Flask Project'
+
+if __name__ == '__main__':
+    
+    ai_api.run(debug = True,port=8080)
